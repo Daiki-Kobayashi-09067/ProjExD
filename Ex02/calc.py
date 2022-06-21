14cfb45598412d3145db33e5c29f721370424dd7
daiki <c0a21050ae@edu.teu.ac.jp>
2022-06-21 15:26:33 +0900

    7

--- Ex02/calc.py
+++ Ex02/calc.py
@@ -4,8 +4,16 @@ import tkinter.messagebox as tkm
 def button_click(event):
     btn=event.widget
     num=btn["text"]
+    if num =="=":
+        eqn=entry.get()
+        res=eval(eqn)
+        entry.delete(0,tk.END)
+        entry.insert(tk.END,res)
+    else:
+        entry.insert(tk.END,num)
+
     #tkm.showinfo("",f"{num}のボタンが押されました")
-    entry.insert(tk.END,num)
+   # entry.insert(tk.END,num)
 
 if __name__ == "__main__":
 
@@ -20,7 +28,7 @@ if __name__ == "__main__":
     entry=tk.Entry(root,justify="right",width=10,font=("Times New Roman",00))
     entry.grid(row=0,column=0,columnspan=3)
 
-    for i,num in enumerate([9,8,7,6,5,4,3,2,1,0,"+"]):
+    for i,num in enumerate([9,8,7,6,5,4,3,2,1,0,"+","="]):
         btn=tk.Button(root,text=f"{num}",width=4,height=2,font=("Times New Roman",30))
         btn.bind("<1>",button_click)
         btn.grid(row=r,column=c)
