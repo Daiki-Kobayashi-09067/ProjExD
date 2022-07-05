96343f73e68e80070ccd254ece2a82254b02642d
daiki <c0a21050ae@edu.teu.ac.jp>
2022-07-05 15:28:27 +0900

    練習7

--- Ex04/dodge_bomb.py
+++ Ex04/dodge_bomb.py
@@ -45,10 +45,20 @@ def main():
         if key_states[pg.K_DOWN]==True: kkimg_rct.centery+=1
         if key_states[pg.K_LEFT]==True: kkimg_rct.centerx-=1
         if key_states[pg.K_RIGHT]==True: kkimg_rct.centerx+=1
+        if check_bound(kkimg_rct,screen_rct) != (1,1):
+            if key_states[pg.K_UP]==True: kkimg_rct.centery+=1
+            if key_states[pg.K_DOWN]==True: kkimg_rct.centery-=1
+            if key_states[pg.K_LEFT]==True: kkimg_rct.centerx+=1
+            if key_states[pg.K_RIGHT]==True: kkimg_rct.centerx-=1
         screen_sfc.blit(kkimg_sfc,kkimg_rct)
         
         #練習6
         bmimg_rct.move_ip(vx,vy)
+
+        #練習7
+        yoko,tate=check_bound(bmimg_rct,screen_rct)
+        vx*=yoko
+        vy*=tate
 
         #練習5
         screen_sfc.blit(bmimg_sfc,bmimg_rct)
@@ -59,6 +69,18 @@ def main():
 
         pg.display.update()
         clock.tick(1000)
+
+#練習7
+def check_bound(rct,scr_rct):
+    #rct:こうかとんor爆弾
+    #scr_rct:スクリーン
+    yoko,tate= +1,+1
+    if rct.left < scr_rct.left or scr_rct.right < rct.right: yoko= -1
+    if rct.top < scr_rct.top or scr_rct.bottom < rct.bottom: tate= -1
+    return yoko,tate
+
+
+
 
 
 
